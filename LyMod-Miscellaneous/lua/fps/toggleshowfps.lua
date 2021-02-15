0ca2c6c401ca9f77a4d0a389f777d941aee66513
Lyon <61743829+LyonMusic@users.noreply.github.com>
2021-02-15 13:09:27 -0300

    Delete LyMod-Miscellaneous directory

--- LyMod-Miscellaneous/lua/fps/toggleshowfps.lua
+++
@@ -1,1 +0,0 @@
-showFps = not showFps
--- LyMod-Miscellaneous/lua/hudmanager.lua
+++
@@ -1,6 +0,0 @@
-local HUDManagerUpdateWaypoints = HUDManager._update_waypoints
-function HUDManager:_update_waypoints(t, dt)
-	HUDManagerUpdateWaypoints(self, t, dt)
-	
-	fpsText = '[' .. math.floor(1/dt) .. ']'
-end
